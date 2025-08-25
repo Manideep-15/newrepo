872fb83840214fb132a81ae0eaf73146eaa793f1
Manideep-15 <138442108+Manideep-15@users.noreply.github.com>
2025-08-25 18:54:01 +0530

    Add files via upload

--- day1/Program.cs
+++ day1/Program.cs
@@ -4,12 +4,27 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
+        Random rand = new Random();
+        int secretNumber = rand.Next(1, 101);
+        int guess;
 
-        Console.Write("Enter your age: ");
-        int age = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine($"Hello {name}, you are {age} years old.");
+        do
+        {
+            Console.Write("Guess the number between 1 and 100: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out guess))
+            {
+                if (guess < secretNumber)
+                    Console.WriteLine("Too low!");
+                else if (guess > secretNumber)
+                    Console.WriteLine("Too high!");
+                else
+                    Console.WriteLine("Correct! You guessed it.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        } while (guess != secretNumber);
     }
 }
