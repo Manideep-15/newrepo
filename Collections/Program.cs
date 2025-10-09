017f6748d356f099742c6f873fa5a9e1f8869524
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-10-09 17:24:15 +0530

    ..

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -5,7 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Collections
-{
+{ 
+
+    class Box<T>
+    {
+       public T Value { get; set; }
+       public void Display()
+       {
+         Console.WriteLine("Value: " + Value);
+       }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -25,15 +35,31 @@ namespace Collections
                 Console.WriteLine($"ID: {student.Key}, Name: {student.Value}");
             }*/
 
-            Stack<int> numbers = new Stack<int>();
-            numbers.Push(10);
-            numbers.Push(20);
-            numbers.Push(30);
-            Console.WriteLine("Top: " + numbers.Peek());
-            while (numbers.Count > 0)
+            /* Stack<int> numbers = new Stack<int>();
+             numbers.Push(10);
+             numbers.Push(20);
+             numbers.Push(30);
+             Console.WriteLine("Top: " + numbers.Peek());
+             while (numbers.Count > 0)
+             {
+                 Console.WriteLine(numbers.Pop());
+             }*/
+
+            /*Queue<string> tasks = new Queue<string>();
+            tasks.Enqueue("Email");
+            tasks.Enqueue("Meeting");
+            tasks.Enqueue("Report");
+
+            while (tasks.Count > 0)
             {
-                Console.WriteLine(numbers.Pop());
-            }
+                Console.WriteLine("Processing: " + tasks.Dequeue());
+            }*/
+
+            Box<int> intBox = new Box<int> { Value = 100 };
+            Box<string> strBox = new Box<string> { Value = "Hello" };
+
+            intBox.Display();
+            strBox.Display();
 
 
         }
