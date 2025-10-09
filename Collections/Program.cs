606c4fccc98000891fd5f4f793974d9c1595f88c
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-10-09 17:17:56 +0530

    ..

---
+++ Collections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*List<string> fruits = new List<string> { "Apple", "Banana", "Cherry" };
+            fruits.Add("Mango");
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            } */
+
+            /*Dictionary<int, string> students = new Dictionary<int, string>();
+            students.Add(1, "Alice");
+            students.Add(2, "Bob");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"ID: {student.Key}, Name: {student.Value}");
+            }*/
+
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(10);
+            numbers.Push(20);
+            numbers.Push(30);
+            Console.WriteLine("Top: " + numbers.Peek());
+            while (numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Pop());
+            }
+
+
+        }
+    }
+}
+
